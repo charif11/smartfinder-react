cd8caa32d545744a6cfe9b382dce979851db6475
s65309010003 <s65309010003@ptniece.ac.th>
2025-03-16 21:15:27 +0700

    Update project

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from "react";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="flex flex-col h-[89vh] justify-center items-center text-3xl">
+      <div className="flex justify-center w-full pb-20">
+        <div className="">
+          <div className="flex items-center">
+            <img
+              className="w-[2rem] mr-2"
+              src="https://cdn-icons-png.flaticon.com/512/3686/3686930.png"
+            />
+            <p>นายชารีฟ หะยีนิเงาะ</p>
+          </div>
+          <div className="flex items-center my-5">
+            <img
+              className="w-[2rem] mr-2"
+              src="https://help.ruk-com.in.th/wp-content/uploads/2022/11/blue-email-box-circle-png-transparent-icon-2.png"
+            />
+            <p>charif.h@rmutsvmail.com</p>
+          </div>
+          <div className="flex items-center">
+            <img
+              className="w-[2rem] mr-2"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/1200px-2023_Facebook_icon.svg.png"
+            />
+            <a
+              href="https://www.facebook.com/nong.charif.846"
+              target="_blank"
+              className="text-blue-400 hover:underline"
+            >
+              Nong Charif
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center w-full border-t-3 pt-20 border-blue-500">
+        <div className="">
+          <div className="flex items-center">
+            <img
+              className="w-[2rem] mr-2"
+              src="https://cdn-icons-png.flaticon.com/512/3686/3686930.png"
+            />
+            <p>นางสาวกัญจน์รัตน์ ผ่องอำไพ</p>
+          </div>
+          <div className="flex items-center my-5">
+            <img
+              className="w-[2rem] mr-2"
+              src="https://help.ruk-com.in.th/wp-content/uploads/2022/11/blue-email-box-circle-png-transparent-icon-2.png"
+            />
+            <p>kancharat.p@rmutsvmail.com</p>
+          </div>
+          <div className="flex items-center">
+            <img
+              className="w-[2rem] mr-2"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/1200px-2023_Facebook_icon.svg.png"
+            />
+            <a
+              href="https://www.facebook.com/kunjarut.pongampai"
+              target="_blank"
+              className="text-blue-400 hover:underline"
+            >
+              Kunjarut Pongampai
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { ArrowRight } from 'lucide-react';
 
 function Home() {
   return (
-    <div>Home CHarif</div>
-  )
+    <div className="flex flex-wrap-reverse relative justify-evenly items-center h-[89vh] px-6 overflow-hidden">
+      <div className="text-3xl">
+        <h1 className="text-5xl border-b-4 border-blue-500 pb-5">
+          Project IOT
+        </h1>
+        <p className="my-5">smart finder</p>
+        <NavLink to="/data" className="btn btn-primary w-[10rem] text-xl">
+          View Data
+          <ArrowRight/>
+        </NavLink>
+      </div>
+      <div className="relative">
+        <div className="bg-red-500 absolute w-full h-full rounded-full top-10 left-5"></div>
+        <img src="home.png" className="-rotate-10" />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
