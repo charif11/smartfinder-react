469c1232e490d85d32b70cc23c9591d7433378e3
charif11 <148172777+charif11@users.noreply.github.com>
2025-03-19 09:34:48 +0700

    Add files via upload

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -77,13 +77,10 @@ function Data() {
       <div className="overflow-x-auto">
         <table className="table">
           <thead>
-            <tr>
+            <tr className="text-white">
               <th>ID</th>
               <th>Latitude</th>
               <th>Longitude</th>
-              <th>Altitude</th>
-              <th>Speed</th>
-              <th>Satellites</th>
               <th>Date</th>
             </tr>
           </thead>
@@ -95,9 +92,6 @@ function Data() {
                   <th>{id}</th>
                   <td>{data.latitude}</td>
                   <td>{data.longitude}</td>
-                  <td>{data.altitude}</td>
-                  <td>{data.speed}</td>
-                  <td>{data.satellites}</td>
                   <td>{new Date(data.timestamp).toLocaleString()}</td>
                 </tr>
               );
